32dc4a6220bcbf96b35a9f7928ecc43823c2a67a
hrfmmymt <hrfmmymt@gmail.com>
2023-07-19 16:52:59 +0900

    feat: use react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC } from 'react';
 import './App.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+
+const TopPage: FC = () => {
+  return <h1>TopPage</h1>;
+};
+const About: FC = () => {
+  return <h1>About</h1>;
+};
+const Page404: FC = () => {
+  return <h1>404</h1>;
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <ul>
+          <li>
+            <NavLink to="/">Top</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+        </ul>
+        <Routes>
+          <Route path="/" element={<TopPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
